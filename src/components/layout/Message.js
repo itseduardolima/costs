f879093eed10b9f991557c0a71dad5b9aaf20b7c
Eduardo Lima <eduardolima2417@gmail.com>
2023-02-24 23:43:19 -0400

    commit

---
+++ src/components/layout/Message.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import styles from './Message.module.css'
+
+function Message({ type, msg }) {
+
+    const [visible, setVisible] = useState(false)
+
+    useEffect(() => {
+        if(!msg) {
+            setVisible(false)
+            return
+        }
+        setVisible(true)
+
+        const timer = setTimeout(() =>{
+            setVisible(false)
+
+
+        return ()  => clearTimeout(timer)
+
+        },3000 )
+
+    }, [msg] )
+
+    return (
+        <>
+            {visible && (
+
+                <div className={`${styles.message}  ${styles[type]} `} >
+                    {msg}
+                </div>
+            )}
+        </>
+    )
+}
+
+export default Message
--- src/index.js
+++ src/index.js
@@ -2,16 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import Projects from './components/pages/Projects';
+import ErrorPage from './components/pages/ErrorPage';
+import NewProject from './components/pages/NewProject';
+import Project from './components/pages/Project';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path:"/",
+    element: <Home/>,
+      },
+      {
+        path:"/contact",
+        element: <Contact/>,
+      },
+      {
+        path:"/company",
+        element: <Company/>,
+      },
+      {
+        path:"/projects",
+        element: <Projects/>,
+      },
+      {
+        path:"/newproject",
+        element: <NewProject/>,
+      },
+      {
+        path:"/project/:id",
+        element: <Project/>,
+      },
+     
+
+    ]
+  },
+  
+])
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
