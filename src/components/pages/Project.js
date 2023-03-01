1602f4ee2c29ee1f4035e5aa41e45f1982cb29a7
Eduardo Lima <eduardolima2417@gmail.com>
2023-03-01 04:36:02 -0400

    commit

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,17 +1,20 @@
+import {parse, v4 as uuidv4} from 'uuid'
 import { useState, useEffect } from 'react'
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import Loading from '../layout/Loading'
 import ProjectForm from '../projects/ProjectForm'
 import Message from '../layout/Message'
+import ServiceForm from '../service/ServiceForm'
 
 function Project() {
 
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
-    const {message, setMessage} = useState()
-    const {type, setType} = useState()
+    const [showServiceForm, setShowServiceForm] = useState(false)
+    const { message, setMessage } = useState()
+    const { type, setType } = useState()
 
     useEffect(() => {
 
@@ -33,7 +36,10 @@ function Project() {
     }, [id])
 
     function editPost(project) {
-        if (project.buget < project.cost) {
+
+
+
+        if (project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
             return false
@@ -59,15 +65,55 @@ function Project() {
             .catch((err) => console.log(err))
     }
 
+    function createService(project) {
+
+        setMessage('')
+
+        const lastService = project.service[project.service.length -1]
+
+        lastService.id = uuidv4()
+
+        const lastServiceCost = lastService.cost
+
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+        if (newCost > parseFloat(project.budget)) {
+            setMessage('Orçamento ultrapassado, verifique o valor do serviço')
+            setType('error')
+            project.service.pop()
+            return false
+        }
+
+        project.cost = newCost
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            //exibir 
+        } )
+        .catch((err) => console.log(err) )
+    }
+    
+
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
         <>
             {project.name ? (
                 <div className={styles.project_details} >
-                    {message && <Message type={type} msg={message}/> }
+                    {message && <Message type={type} msg={message} />}
                     <div className={styles.details_container} >
                         <h1>{project.name}</h1>
 
@@ -99,7 +145,27 @@ function Project() {
                         )}
                     </div>
 
+                    <div className={styles.service_form_container}>
+                        <h2>Adicione um serviço:</h2>
+                        <button className={styles.btn} onClick={toggleServiceForm}>
+                            {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                        </button>
+                        <div className={styles.project_info}>
+                            {showServiceForm &&  (
+                                <ServiceForm  
+                                handleSubmit={createService}
+                                btnText="Adicionar serviço"
+                                projectData={project}
+                                 />
+                            )}
+                        </div>
+                    </div>
+                    <h2>Serviços</h2>
+                    <div className={styles.details_container} >
+                        <p>Itens de Serviços</p>
+                    </div>
                 </div>
+                
             ) : (
                 <Loading />
             )}
